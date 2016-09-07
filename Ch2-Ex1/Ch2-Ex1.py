a845e677f612833a589c65f780fe7b79d45eed66
daydayl <tiantian345@hotmail.com>
2016-09-06 22:08:45 -0400

    Chapter 2, Exercise 1

---
+++ Ch2-Ex1/Ch2-Ex1.py
@@ -0,0 +1,20 @@
+# name: Tiantian Liu
+# email: til43@pitt.edu
+# date: Sept. 06. 2016
+# class: CS0008-f2016
+# instructor: Max Novelli (man8@pitt.edu)
+#
+# Description:
+# Chapter2, Exercise 1
+
+# assign variables with values
+name = 'Tiantian Liu'
+address = 'Centre Ave, Pittsburgh, Pennsylvania, 15213'
+telephone = '4124194135'
+major = 'Math'
+
+# show information on the screen
+print "Name:", name
+print "Address:", address
+print "Telephone:", telephone
+print "Major:", major
