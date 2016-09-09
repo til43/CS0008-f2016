0b2f52c3058e2cd7fc93712aa9ac9165c9460bc2
daydayl <tiantian345@hotmail.com>
2016-09-09 13:00:22 -0400

    Chapter 2, Exercise 3

---
+++ Ch2-Ex3/ch2-ex3.py
@@ -0,0 +1,14 @@
+# name: Tiantian Liu
+# email: til43@pitt.edu
+# date: Sept. 08. 2016
+# class: CS0008-f2016
+# instructor: Max Novelli (man8@pitt.edu)
+# Description:
+# Chapter2, Exercise 3
+
+#assign squaremeters with input
+a = input('enter square meters:')
+acre = float(4046.8564224) * float(a)
+print a, 'square meters', '=', acre, 'acre'
+
+
