2287fb8bf89429d06c1829811ffc87a17d8143bf
daydayl <tiantian345@hotmail.com>
2016-09-11 18:48:50 -0400

    Chapter 2, Exercise 5

---
+++ Ch2-Ex5/Cha-Ex5.py
@@ -0,0 +1,24 @@
+# name: Tiantian Liu
+# email: til43@pitt.edu
+# date: Sept. 11. 2016
+# class: CS0008-f2016
+# instructor: Max Novelli (man8@pitt.edu)
+#
+# Description:
+# Chapter2, Exercise 5
+
+
+v = 90
+t = 6
+s = v * t
+print "The distance the car will travel in", t, "hours:", s
+t = 10
+s = v * t
+print "The distance the car will travel in", t, "hours:", s
+t = 15
+s = v * t
+print "The distance the car will travel in", t, "hours:", s
+t = 2 + (5/12)
+s = v * t
+print "The distance the car will travel in", t, "hours:", s
+
