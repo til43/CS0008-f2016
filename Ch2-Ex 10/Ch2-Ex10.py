8b5477773a79e5088f5f7e4d6f32162aed2b55ad
daydayl <tiantian345@hotmail.com>
2016-09-12 12:12:43 -0400

    Chapter 2, Exercise 10

---
+++ Ch2-Ex 10/Ch2-Ex10.py
@@ -0,0 +1,14 @@
+# name: Tiantian Liu
+# email: til43@pitt.edu
+# date: Sept. 12. 2016
+# class: CS0008-f2016
+# instructor: Max Novelli (man8@pitt.edu)
+# Description:
+# Chapter2, Exercise 10
+
+cookies = input("Number of cookies you want to make: ")
+sugar = 300 * (cookies/48)
+butters = 240 * (cookies/48)
+flour = 330 * (cookies/48)
+print "To make", cookies, "cookies,", "you need:", sugar, "gr of sugar;",\
+    butters, "gr of butters;", flour, "gr of flours."
