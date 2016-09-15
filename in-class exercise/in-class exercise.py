e6ada8587a567267e0e80bda3bd0834958715bfb
daydayl <tiantian345@hotmail.com>
2016-09-15 12:42:21 -0400

    in-class exercise

---
+++ in-class exercise/in-class exercise.py
@@ -0,0 +1,16 @@
+# name: Tiantian Liu
+# email: til43@pitt.edu
+# date: Sept. 15. 2016
+# class: CS0008-f2016
+# instructor: Max Novelli (man8@pitt.edu)
+# Description:
+# in-class exercise
+
+a = input("input a number:")
+if a > 0:
+    print a * 3
+elif a == 0:
+    print "What a cool number"
+else:
+    print a + 5
+    
