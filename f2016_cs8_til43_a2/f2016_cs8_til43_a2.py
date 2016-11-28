8ff2f2e327ca5023dd723145fdb259fd538c02e7
nitrosx (Max Novelli) <man8@pitt.edu>
2016-11-28 00:14:46 -0500

    fixes and comments on assignment #2

--- f2016_cs8_til43_a2/f2016_cs8_til43_a2.py
+++ f2016_cs8_til43_a2/f2016_cs8_til43_a2.py
@@ -15,13 +15,16 @@
 total_lines = 0
 total_distance = 0.00
 
+# MN: where are the function input arguments?
 # define processFile function
 def processfile():
 
+    # MN: DO NOT USE global variable unless it is absolutely necessary
     #global variable
     global total_lines
     global total_distance
 
+    # MN: where is file_name defined? which is the variable scope?
     #open the file chosen
     infile = open(file_name, 'r')
     printKV('\nFile to be read:', file_name)
@@ -41,6 +44,7 @@ def processfile():
     printKV('Partial Total # of lines:', partial_sum_lines, 25)
     printKV('Partial distance run:',partial_sum_distance, 25)
 
+    # MN: these statements should not be in this function but in the main loop
     #get the total number of lines
     total_lines += partial_sum_lines
     total_distance += partial_sum_distance
@@ -69,13 +73,25 @@ def printKV(key, value, klen=0):
 #ask users for the file names
 file_name = input("enter valid file name choose file, enter 'quit' if you finish: ")
 
+# MN: this loop is not an infinite loop
+# MN: what happens if I provide a file with a different name?
+#     lines and distances do not get counted
+# MN: I would use the following loop
 #loops indefinitely
-while file_name == 'f2016_cs8_a2.data.1.csv' or file_name == 'f2016_cs8_a2.data.2.csv':
-    if file_name =='f2016_cs8_a2.data.1.csv':
-        processfile()
-    else:
-        file_name = 'f2016_cs8_a2.data.2.csv'
-        processfile()
+#while file_name == 'f2016_cs8_a2.data.1.csv' or file_name == 'f2016_cs8_a2.data.2.csv':
+while file_name and file_name != "q" and file_name != "quit":
+    # MN: what if I provide in input a file that is named "test.csv" and is a valid file?
+    #     I would not use this conditional statement testing on the file names
+    #     but just on the exit values aka sentinels
+    #if file_name =='f2016_cs8_a2.data.1.csv':
+    #    processfile()
+    #else:
+    #    file_name = 'f2016_cs8_a2.data.2.csv'
+    #    processfile()
+
+    # MN: process the file
+    processfile()
+    # MN: ask for the next file
     file_name = input("enter '1'or '2'to choose file, enter 'quit' if you finish: ")
 
 #if the user choose quit, compute the total result
@@ -84,16 +100,6 @@ else:
     print('\nTotals')
     printKV('Total # of lines:', total_lines, 25)
     printKV('Total distance run:', total_distance, 25)
-
-
-
-
-
-
-
-
-
-
 
 
 
